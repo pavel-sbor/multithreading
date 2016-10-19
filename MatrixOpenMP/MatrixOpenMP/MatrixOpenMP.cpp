1df61abb6b6bf97737f7e91ade08a1422d6a3839
Pavel <pavel-sbor@rambler.ru>
2016-10-20 00:13:47 +0300

    Initial commit

---
+++ MatrixOpenMP/MatrixOpenMP/MatrixOpenMP.cpp
@@ -0,0 +1,14 @@
+#include "stdafx.h"
+
+#include<iostream>
+#include<omp.h>
+#include <boost/log/trivial.hpp>
+using namespace std;
+int main(int argc, char *argv[])
+{
+	if (argc != 2) {
+		
+	}
+    return 0;
+}
+
